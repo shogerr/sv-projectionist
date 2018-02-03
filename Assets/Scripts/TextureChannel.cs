ddb14dae4ea6773600f2ce5b6ab20f043eb628fe
shogerr <shogerr@oregonstate.edu>
2018-02-02 21:54:45 -0800

    add project contents

---
+++ Assets/Scripts/TextureChannel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TextureChannel : NetworkBehaviour {
+    // Sending texture
+    public Texture2D texture_;
+
+    // Recieving texture
+    private Texture2D _texture;
+
+	// Update is called once per frame
+	void Update () {
+        if (Network.isServer)
+        {
+            if(Input.GetKeyDown(KeyCode.X))
+            {
+                RpcSend(texture_.EncodeToPNG());
+            }
+        }
+	}
+    [ClientRpc]
+    private void RpcSend(byte[] r)
+    {
+        _texture = new Texture2D(1, 1);
+        _texture.LoadImage(r);
+    }
+}
