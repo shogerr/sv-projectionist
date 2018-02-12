41754e13a9097307fdfff794fbd9dc2eea08be2f
shogerr <shogerr@oregonstate.edu>
2018-02-12 00:13:32 -0800

    add udp example

---
+++ Assets/Scripts/UDPSend.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class UDPSend : MonoBehaviour {
+    private static int local_port;
+    private string IP;
+    public int port;
+
+    IPEndPoint remoteEndPoint;
+    UdpClient client;
+
+    public void init()
+    {
+        IP = "127.0.0.1";
+        port = 8051;
+
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+        client = new UdpClient();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sendString("testing");
+	}
+
+    public void sendString(string msg)
+    {
+        try
+        {
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            client.Send(data, data.Length, remoteEndPoint);
+        }
+        catch (Exception err)
+        {
+            print(err.ToString());
+        }
+    }
+}
