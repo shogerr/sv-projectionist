8e55e7a6abc816053a614343150fd4ef9520f4f3
shogerr <shogerr@oregonstate.edu>
2018-02-24 14:40:20 -0800

    add viewport texture

--- Assets/Scripts/UnwrapObject.cs
+++ Assets/Scripts/UnwrapObject.cs
@@ -8,38 +8,34 @@ public class UnwrapObject : MonoBehaviour {
     public void GenerateUVs()
     {
         var meshes = target.GetComponentsInChildren<MeshFilter>();
-
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < 100; j++)
         {
-            Mesh mesh = meshes[1].mesh;
-            Vector3[] vertices = mesh.vertices;
-            Vector3[] normals = mesh.normals;
-            Vector2[] uvs = new Vector2[vertices.Length];
-            int i = 0;
-            while (i < uvs.Length)
-            {
-                /*
-                if (Mathf.Abs(normals[i].y) > 0.5f)
-                {  // if normal is like vector3.up
-                    uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
-                }
-                else if (Mathf.Abs(normals[i].x) > 0.5f)
-                {  // if normal is like vector3.right
-                    uvs[i] = new Vector2(vertices[i].z, vertices[i].y);
-                }
-                */
-                if (Mathf.Abs(normals[i].z) > .5f)
-                { // last case if it's like vector3.forward
-                    uvs[i] = new Vector2(vertices[i].x, vertices[i].y);
-                }
-                i++;
-            }
-            mesh.uv = uvs;
-            foreach (var uv in uvs)
-            {
-                Debug.Log(uv);
-            }
-            //meshes[i].sharedMesh.uv = Unwrapping.GeneratePerTriangleUV(meshes[i].sharedMesh);
+            Unwrapping.GenerateSecondaryUVSet(meshes[j].sharedMesh);
+            meshes[j].sharedMesh.uv = meshes[j].sharedMesh.uv2;
         }
+    }
+    public void GenerateRawUV(Mesh mesh)
+    {
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+        Vector2[] uvs = new Vector2[vertices.Length];
+        int i = 0;
+        while (i < uvs.Length)
+        {
+            if (Mathf.Abs(normals[i].y) > 0.5f)
+            {  // if normal is like vector3.up
+                uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+            }
+            else if (Mathf.Abs(normals[i].x) > 0.5f)
+            {  // if normal is like vector3.right
+                uvs[i] = new Vector2(vertices[i].z, vertices[i].y);
+            }
+            else
+            { // last case if it's like vector3.forward
+                uvs[i] = new Vector2(vertices[i].x, vertices[i].y);
+            }
+            i++;
+        }
+        mesh.uv = uvs;
     }
 } 
